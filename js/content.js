4f78652b1dee39ee9baa50e504ff05854e92ac93
tanzhijian <i@tanzhijian.org>
2019-03-19 21:48:27 +0800

    添加一行判定，避免非 video 页面报错

--- js/content.js
+++ js/content.js
@@ -1,11 +1,14 @@
 function bilingualSubtitles() {
   // 开启双语字幕
   // 由于 Coursera 使用的是 video tag，所以直接找到节点打开对应的字幕
-  const languages = new Set(['zh-CN', 'en']);
-  let video = document.getElementById('c-video_html5_api');
-  for (let i = 0; i < video.textTracks.length; i++) {
-    if (languages.has(video.textTracks[i].language)) {
-      video.textTracks[i].mode = 'showing';
+  // 添加一行判定，避免非 video 页面报错
+  if (document.getElementById('c-video_html5_api')) {
+    const languages = new Set(['zh-CN', 'en']);
+    let video = document.getElementById('c-video_html5_api');
+    for (let i = 0; i < video.textTracks.length; i++) {
+      if (languages.has(video.textTracks[i].language)) {
+        video.textTracks[i].mode = 'showing';
+      }
     }
   }
 }
