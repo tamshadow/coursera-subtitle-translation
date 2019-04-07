0c6b5e9753b850821365e89f71d17daf3e5e236d
tanzhijian <i@tanzhijian.org>
2019-04-07 15:19:42 +0800

    减少了翻译 API 的请求次数

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ function openBilingual () {
   let tracks = document.getElementsByTagName('track')
   let enTrack
   let zhcnTrack
-  if (tracks.length > 0) {
+  if (tracks.length) {
     // 1. 遍历字幕节点，找到中英文字幕
     for (let i = 0; i < tracks.length; i++) {
       if (tracks[i].srclang === 'en') {
@@ -21,14 +21,35 @@ function openBilingual () {
         zhcnTrack.track.mode = 'showing'
       } else {
         // 4. 如果不存在，开启翻译
-        // 遍历出英文字幕的所有文本内容，然后逐句翻译
-        // 这样会大量调用翻译 API，不大优雅，不过暂时先这样
-        cues = enTrack.track.cues
+        // 由于逐句翻译会大量请求翻译 API，需要减少请求次数
+        // 取出英文字幕的所有文本内容，整合成为一个列表，
+        // 每项为不大于 5000 字的字符串，
+        // （好像目前使用的这个 API 有 5000 字上限？）
+        // 以及它在 cues 的起始位置
+        // 数据结构大概是 [[0, 文本], [95, 文本]]
+        let cues = enTrack.track.cues
+        let cuesTextList = []
         for (let i = 0; i < cues.length; i++) {
-          getTranslation(cues[i].text, responseText => {
-            cues[i].text = cues[i].text + '\n' + responseText
+          if (cuesTextList.length &&
+              cuesTextList[cuesTextList.length - 1][1].length +
+              cues[i].text.length < 5000) {
+            // 需要插入一个分隔符(换行)，以便之后为翻译完的字符串 split
+            cuesTextList[cuesTextList.length - 1][1] += '\n' + cues[i].text
+          } else {
+            cuesTextList.push([i, cues[i].text])
+          }
+        }
+        // 进行翻译
+        for (let i = 0; i < cuesTextList.length; i++) {
+          getTranslation(cuesTextList[i][1], responseText => {
+            // 取得返回的文本，根据之前插入的换行符 split
+            // 然后确定所在 cues 文本的序列，为之前存储的起始位置 + 目前的相对位置
+            // 把翻译后的文本直接添加到英文字幕后面
+            const translatedTextList = responseText.split('\n')
+            for (let j = 0; j < translatedTextList.length; j++) {
+              cues[cuesTextList[i][0] + j].text += '\n' + translatedTextList[j]
+            }
           })
-          // break
         }
       }
     }
@@ -36,8 +57,9 @@ function openBilingual () {
 }
 
 function getTranslation (sentence, callback) {
+  // 通过谷歌翻译 API 进行翻译，输入待翻译的字符串，返回翻译完成的字符串
   const xhr = new XMLHttpRequest()
-  let url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=zh&dt=t&q=${sentence}`
+  let url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=zh&dt=t&q=${encodeURI(sentence)}`
   xhr.open('GET', url, true)
   xhr.responseType = 'text'
   xhr.onload = function () {
